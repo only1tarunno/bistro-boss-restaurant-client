6c10318fe1625329f6598efb81a57f5d00794931
only1tarunno <tarunno281@gmail.com>
2023-11-11 10:18:56 +0600

    load reviews

--- src/pages/Home/Testimonial.jsx
+++ src/pages/Home/Testimonial.jsx
@@ -15,7 +15,7 @@ const Testimonial = () => {
   const [reviews, setreviviews] = useState([]);
 
   useEffect(() => {
-    fetch("reviews.json")
+    fetch("http://localhost:5000/reviews")
       .then((res) => res.json())
       .then((data) => setreviviews(data));
   }, []);
--- src/pages/Shop/ShopItemtab.jsx
+++ src/pages/Shop/ShopItemtab.jsx
@@ -1,11 +1,38 @@
 import Foodcard from "../../components/Foodcard";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "./slider.css";
+// import required modules
+import { Pagination } from "swiper/modules";
+
 const ShopItemtab = ({ items }) => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
   return (
-    <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-      {items.map((item) => (
-        <Foodcard key={item._id} item={item}></Foodcard>
-      ))}
+    <div>
+      <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {items.map((item) => (
+              <Foodcard key={item._id} item={item}></Foodcard>
+            ))}
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
     </div>
   );
 };
