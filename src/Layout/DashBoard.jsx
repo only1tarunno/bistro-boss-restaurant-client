ee60a1753f1221294b62302373037f072757cca9
only1tarunno <tarunno281@gmail.com>
2023-11-14 00:13:31 +0600

    dashboard cart page add

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -1,51 +1,59 @@
 import { NavLink, Outlet } from "react-router-dom";
-import Navbar from "../pages/shared/Navbar";
 import { AiFillHome, AiFillCalendar, AiFillStar } from "react-icons/ai";
 import { BsFillCalendarHeartFill, BsFillCartFill } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
 import "./dashboard.css";
 
 const DashBoard = () => {
   return (
-    <>
-      <Navbar></Navbar>
-      <div className="flex">
-        {/* dashboard sidebar  */}
-        <div className="w-64 pt-24 bg-orange-400 min-h-screen">
-          <ul className="menu dashboard-menu space-y-2 capitalize">
-            <li>
-              <NavLink to="/dashboard/userHome">
-                <AiFillHome></AiFillHome> User Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/dashboard/reservation">
-                <AiFillCalendar></AiFillCalendar> Reservation
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/dashboard/cart">
-                <BsFillCartFill></BsFillCartFill> My Cart
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/dashboard/rating">
-                <AiFillStar></AiFillStar> rating
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/dashboard/bookings">
-                <BsFillCalendarHeartFill></BsFillCalendarHeartFill> my bookings
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-
-        {/* dashboard content  */}
-        <div className="pt-24 flex-1">
-          <Outlet></Outlet>
-        </div>
+    <div className="flex">
+      {/* dashboard sidebar  */}
+      <div className="w-64 pt-10 bg-orange-400 min-h-screen">
+        <ul className="menu dashboard-menu space-y-2 capitalize">
+          <li>
+            <NavLink to="/dashboard/userHome">
+              <AiFillHome></AiFillHome> User Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/reservation">
+              <AiFillCalendar></AiFillCalendar> Reservation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/cart">
+              <BsFillCartFill></BsFillCartFill> My Cart
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/rating">
+              <AiFillStar></AiFillStar> rating
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/bookings">
+              <BsFillCalendarHeartFill></BsFillCalendarHeartFill> my bookings
+            </NavLink>
+          </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <AiFillHome></AiFillHome> Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/menu">
+              <GiHamburgerMenu></GiHamburgerMenu> Menu
+            </NavLink>
+          </li>
+        </ul>
       </div>
-    </>
+
+      {/* dashboard content  */}
+      <div className=" flex-1 p-10">
+        <Outlet></Outlet>
+      </div>
+    </div>
   );
 };
 
--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,7 +1,71 @@
+import useCart from "../../../hooks/useCart";
+import SharedSectionTitle from "../../../components/SharedSectionTitle";
+import { FaTrash } from "react-icons/fa6";
+
 const Cart = () => {
+  const [cart] = useCart();
+  const handleDelete = (id) => {
+    console.log(id);
+  };
+
   return (
     <div>
-      <h2 className="text-6xl">my cart</h2>
+      <SharedSectionTitle
+        heading={"Wanna Add More"}
+        subHeading={"---My Cart---"}
+      ></SharedSectionTitle>
+      <div className="pt-10">
+        <div className="flex justify-between pb-8">
+          <h2 className="text-4xl">Total Items: {cart.length}</h2>
+          <h2 className="text-4xl">
+            Total Price:{" "}
+            {cart.reduce((sum, item) => sum + item.price, 0).toFixed(2)}
+          </h2>
+          <div>
+            <button className="btn btn-primary">Pay</button>
+          </div>
+        </div>
+        <div className="overflow-x-auto">
+          <table className="table text-center">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Sl No.</th>
+                <th>Item Image</th>
+                <th>Item Name</th>
+                <th>Price</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item, index) => (
+                <tr key={item._id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div className="flex items-center justify-center gap-3">
+                      <div className="avatar">
+                        <div className="rounded-full w-12 h-12">
+                          <img src={item?.image} alt={item?.name} />
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{item?.name}</td>
+                  <td>{item?.price}</td>
+                  <td>
+                    <button
+                      onClick={() => handleDelete(item._id)}
+                      className="btn btn-sm btn-secondary"
+                    >
+                      <FaTrash></FaTrash>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
